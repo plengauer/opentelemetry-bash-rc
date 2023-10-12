3e7764c77ae15e17375c9f8771d43142c1fe968e
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-12 21:49:16 +0200

    adjust default service name

--- src/etc/profile.d/opentelemetry_shell_interactive.sh
+++ src/etc/profile.d/opentelemetry_shell_interactive.sh
@@ -1,5 +1,5 @@
 if [ -n "$PS1" ]; then
-  export OTEL_SERVICE_NAME="Interactive Session @ $(cat /etc/hostname)"
+  export OTEL_SERVICE_NAME="Interactive Session"
   . /var/lib/opentelemetry_shell_interactive/env
   . /usr/bin/opentelemetry_shell.sh
 fi
