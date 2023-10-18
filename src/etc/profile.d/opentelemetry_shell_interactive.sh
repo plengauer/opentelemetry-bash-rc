fa4f3e26db76b13a058a085c7899f859bb0c0edc
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-18 22:26:11 +0200

    Update opentelemetry_shell_interactive.sh

--- src/etc/profile.d/opentelemetry_shell_interactive.sh
+++ src/etc/profile.d/opentelemetry_shell_interactive.sh
@@ -1,5 +1,9 @@
-if [ -n "$PS1" ]; then
-  export OTEL_SERVICE_NAME="Interactive Session"
-  . /var/lib/opentelemetry_shell_interactive/env
-  . /usr/bin/opentelemetry_shell.sh
-fi
+case "$-" in
+  *i*)
+    export OTEL_SERVICE_NAME="Interactive Session"
+    . /var/lib/opentelemetry_shell_interactive/env
+    . /usr/bin/opentelemetry_shell.sh
+    ;;
+  *)
+    ;;
+esac
